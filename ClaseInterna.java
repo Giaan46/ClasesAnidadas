8c2fbc60ca2fa3d882a6f4bc4ca323e542763849
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-30 11:17:47 +0100

    Add files via upload

---
+++ ClaseInterna.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class ClaseInterna {
+    public static void main(String[] args) {
+        Order order = new Order();
+        Order.OrderDitail orderDetail = order.new OrderDitail();
+
+
+
+
+
+    }
+}
+//motherlode
